373c79f09cf0252fb88571c782b35a02f0cb0f15
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-03-19 13:44:58 +0800

    implements OnInit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,30 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+
+//oninit is like useeffect that run once after the component mounts
+//lightbulb then implement oninit
+export class AppComponent implements OnInit {
+  //inject http so we can use it
+  http = inject(HttpClient);
   title = 'Dating App';
+  users: any;
+
+  ngOnInit(): void {
+    //use 'this.' when using class property
+    //all observables need subscribe
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      //what will happen next after theres a return
+      //put the respose to the class property 'users'
+      next: (response) => (this.users = response),
+
+      //what will happen if theres error
+      error: (error) => {
+        console.log(error);
+      },
+
+      //what will happen after next
+      complete: () => console.log('Request has completed' + this.users),
+    });
+  }
 }
