f69b95dfa05ed916ef6a8a924cd36c5f87086134
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-03-20 16:39:30 +0800

    password hash and salt

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,6 +40,31 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        //firstordefault looks for user and if theres none it returns null
+        var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+        //if user does not exist
+        if (user == null) return Unauthorized("Invalid Username");
+
+        //if user exist, pass the key/salt to hmac
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        //hash the password from user logging in using the salt from existing user
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        //loop as long as the i is smaller than the length
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            //compare each byte array values to see if theres difference
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
+
+        return user;
+    }
+
     //private method that will only be used in this class
     //will return bool
     //Task is used since its async
