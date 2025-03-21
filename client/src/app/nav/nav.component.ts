c0adfa611274141a4e2d32f8d3149ac3d5e44f21
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-03-21 17:36:55 +0800

    form setup

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  //formsmodule to use form
+  imports: [FormsModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  //this is the model.username, model.password in template
 
+  model: any = {};
+
+  //angular methods
+  login() {
+    console.log(this.model);
+  }
 }
