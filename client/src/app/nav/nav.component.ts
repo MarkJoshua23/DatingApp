d633eb5481d3a96985ce07abbd44ff1e85fc2761
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-03-24 13:15:39 +0800

    conditional rendering

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,28 +3,31 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 
 @Component({
-  selector: 'app-nav',
-  //formsmodule to use form
-  imports: [FormsModule],
-  templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css',
+    selector: 'app-nav',
+    //formsmodule to use form
+    imports: [FormsModule],
+    templateUrl: './nav.component.html',
+    styleUrl: './nav.component.css',
 })
 export class NavComponent {
-  private accountService = inject(AccountService);
-  loggedIn = false;
-  //this is the model.username, model.password in template
-  model: any = {};
+    private accountService = inject(AccountService);
+    loggedIn = false;
+    //this is the model.username, model.password in template
+    model: any = {};
 
-  //angular methods
-  login() {
-    //the services returns an observable not the values
-    //subscribe to the returned observable
-    this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.loggedIn = true;
-      },
-      error: (error) => console.log(error),
-    });
-  }
+    //angular methods
+    login() {
+        //the services returns an observable not the values
+        //subscribe to the returned observable
+        this.accountService.login(this.model).subscribe({
+            next: (response) => {
+                console.log(response);
+                this.loggedIn = true;
+            },
+            error: (error) => console.log(error),
+        });
+    }
+    logout() {
+        this.loggedIn = false;
+    }
 }
