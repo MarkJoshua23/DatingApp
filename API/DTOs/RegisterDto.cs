49ecf86d4452af6cf648d928244c1b711fc6d4f7
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-03-25 16:59:53 +0800

    add controller

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,9 +5,11 @@ namespace API.DTOs;
 
 public class RegisterDto
 {
-    public required string Username { get; set; }
-
-    //other way of making a prop required
     [Required]
-    public required string Password { get; set; }
+    public string Username { get; set; } = string.Empty;
+
+    //better validation error is using required is using the []
+    [Required]
+    [StringLength(8, MinimumLength = 4)]
+    public string Password { get; set; } = string.Empty;
 }
