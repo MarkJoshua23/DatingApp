c3c25f6ae73581cbd377cdfb125d550bdcdf5d83
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-03-25 13:52:51 +0800

    route guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'members', component: MemberListComponent},
+    //the user need to satisfy the guard conditions to access the members page
+    {path: 'members', component: MemberListComponent, canActivate:[authGuard]},
     {path: 'members/:id', component: MemberDetailComponent},
     {path: 'lists', component: ListsComponent},
     {path: 'messages', component: MessagesComponent},
