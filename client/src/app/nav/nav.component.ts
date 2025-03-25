76397c33b24deb193aab23c1eb225f32a9859f3f
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2025-03-25 10:43:56 +0800

    programatical routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -15,23 +15,28 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
-    //use this service to use login or access the currentuser state
+  //use this service to use login or access the currentuser state
   accountService = inject(AccountService);
   //this is the model.username, model.password in template
   model: any = {};
+
+  //switch routes programatically
+  private router = inject(Router);
 
   //angular methods
   login() {
     //the services returns an observable not the values
     //subscribe to the returned observable
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log('user', response);
+      next: () => {
+        //navigate the user to members component
+        this.router.navigateByUrl('/members');
       },
       error: (error) => console.log(error),
     });
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
